db808565c353eaea333b1002273efd7bb2716428
yuio7279 <yuio7279@gmail.com>
2023-06-23 16:19:34 +0900

    init commit

---
+++ src/main/java/com/sprata/jpaadvanceprac/JpaAdvancePracApplication.java
@@ -0,0 +1,13 @@
+package com.sprata.jpaadvanceprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAdvancePracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAdvancePracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprata/jpaadvanceprac/JpaAdvancePracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprata.jpaadvanceprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAdvancePracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
