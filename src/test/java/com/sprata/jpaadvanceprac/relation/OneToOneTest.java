6f6da0149aa18386c84f0d07183c300d2d8ae971
yuio7279 <yuio7279@gmail.com>
2023-06-23 16:34:46 +0900

    1:1 단방향 조회 테스트

--- src/test/java/com/sprata/jpaadvanceprac/relation/OneToOneTest.java
+++ src/test/java/com/sprata/jpaadvanceprac/relation/OneToOneTest.java
@@ -7,6 +7,7 @@ import com.sprata.jpaadvanceprac.repository.FoodRepository;
 import com.sprata.jpaadvanceprac.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
@@ -37,5 +38,16 @@ public class OneToOneTest {
         foodRepository.save(food);
         food.setUser(user);
     }
+
+    @Test
+    @Rollback(value = false)
+    @DisplayName("1:1 단방향 조회 테스트")
+    void test2(){
+        //단방향에서는 기준 Entity만 다른 Entity를 조회할 수 있다.
+        Food food = foodRepository.findById(1L).orElseThrow(NullPointerException::new);
+        System.out.println("food.getUser().getName() = " + food.getUser().getName());
+
+    }
+
 
 }
