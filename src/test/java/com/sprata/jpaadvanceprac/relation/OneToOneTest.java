b75443d5d652277b8b2e3489b9c9d6065a35d9da
yuio7279 <yuio7279@gmail.com>
2023-06-23 16:54:58 +0900

    1:1 양방향 조회 테스트

--- src/test/java/com/sprata/jpaadvanceprac/relation/OneToOneTest.java
+++ src/test/java/com/sprata/jpaadvanceprac/relation/OneToOneTest.java
@@ -7,7 +7,6 @@ import com.sprata.jpaadvanceprac.repository.FoodRepository;
 import com.sprata.jpaadvanceprac.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.internal.matchers.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
@@ -70,6 +69,28 @@ public class OneToOneTest {
 
     }
 
+    @Test
+    @DisplayName("1:1 양방향 조회 테스트 유저 기준")
+    void test4(){
+        //양방향에서는 user또한 조회가 가능하다.
+        User user = userRepository.findById(1L).orElseThrow(NullPointerException::new);
+        //고객 정보 조회
+        System.out.println("user.getName() = " + user.getName());
+        //고객이 주문한 음식 조회
+        System.out.println("user.getFood().getName() = " + user.getFood().getName());
+        System.out.println("user.getFood().getPrice() = " + user.getFood().getPrice());
+    }
 
+    @Test
+    @DisplayName("1:1 양방향 조회 테스트 음식 기준")
+    void test5(){
+        Food food = foodRepository.findById(1L).orElseThrow(NullPointerException::new);
+        //고객 정보 조회
+        System.out.println("food.getUser().getName() = " + food.getUser().getName());
 
+        //고객이 주문한 음식 조회
+        System.out.println("food.getName() = " + food.getName());
+        System.out.println("food.getPrice() = " + food.getPrice());
+
+    }
 }
